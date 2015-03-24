5d9610283dd25edddac98551e831a85e726256b9
bennlich <bennlich@gmail.com>
2015-03-23 22:06:00 -0700

    Remove build files and old source files (about to be moved)

--- client/src/directives/leafletMap.js
+++
@@ -1,6 +0,0 @@
-window.angular.module("seismoApp")
-.directive("leafletMap", ["map", function(map) {
-  return function(scope, element, attrs) {
-    map.init(attrs.id);
-  };
-}]);
--- client/src/services/query.js
+++
@@ -1,9 +0,0 @@
-window.angular.module("seismoApp")
-.service("query", [function() {
-  var serverUrl = "http://localhost:3000";
-  return {
-    path: function(path) {
-      return serverUrl + path;
-    }
-  };
-}]);
