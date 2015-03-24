9fe7c74be29677b243c3e4fe8893ae0f4fd0baec
bennlich <bennlich@gmail.com>
2015-03-24 14:04:19 -0700

    More nubbins renaming

--- client/src/SeismoApp.js
+++ client/src/SeismoApp.js
@@ -5,16 +5,16 @@ var angular = window.angular;
 import { SeismoMain } from "./SeismoMain.ctrl.js";
 import { SeismoMap } from "./map/SeismoMap.svc.js";
 import { SeismoQuery } from "./SeismoQuery.svc.js";
-import { SeismoTimeFilter } from "./seismo-time-filter/SeismoTimeFilter.dir.js";
-import { SeismoFilters } from "./seismo-filters/SeismoFilters.dir.js";
+import { SeismoTimeNubbin } from "./seismo-query-nubbins/SeismoTimeNubbin.dir.js";
+import { SeismoQueryNubbins } from "./seismo-query-nubbins/SeismoQueryNubbins.dir.js";
 import { LeafletMap } from "./map/LeafletMap.dir.js";
 
 angular.module("SeismoApp", [])
   .controller("SeismoMain", SeismoMain)
   .service("SeismoMap", SeismoMap)
   .service("SeismoQuery", SeismoQuery)
-  .directive("seismoTimeFilter", SeismoTimeFilter)
-  .directive("seismoFilters", SeismoFilters)
+  .directive("seismoTimeNubbin", SeismoTimeNubbin)
+  .directive("seismoQueryNubbins", SeismoQueryNubbins)
   .directive("leafletMap", LeafletMap)
   .run([function() {
     console.log("Seismo is running.");
