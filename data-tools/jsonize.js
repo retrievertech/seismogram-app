738f6beb96e9968d2dfcab25d926ed9d9d3050ba
Marius Nita <m@mariusnita.com>
2015-03-24 16:38:31 -0600

    generate random statuses for testing

--- data-tools/jsonize.js
+++ data-tools/jsonize.js
@@ -1,5 +1,6 @@
 var stationsModule = require("./stations");
 var filesModule = require("./files");
+var statusesModule = require("./statuses");
 var fs = require("fs");
 var path = require("path");
 
@@ -7,7 +8,9 @@ stationsModule.parse("data/stations.csv", function(stations) {
   var files = filesModule.parse("data/files.uniq.txt");
   files = filesModule.filterBadStations(files, stations);
   stationsModule.populateSeismoData(stations, files);
+  var statuses = statusesModule.generateRandomStatuses(files);
   fs.writeFileSync(path.join(__dirname, "files.json"), JSON.stringify(files, null, 2));
   fs.writeFileSync(path.join(__dirname, "stations.json"), JSON.stringify(stations, null, 2));
-  console.log("Wrote output to files.json and stations.json");
+  fs.writeFileSync(path.join(__dirname, "statuses.json"), JSON.stringify(statuses, null, 2));
+  console.log("Wrote output to files.json, stations.json, and statuses.json.");
 });
