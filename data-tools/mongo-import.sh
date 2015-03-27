d23e13c61c396d27d94e0c981e6003543b007478
Marius Nita <m@mariusnita.com>
2015-03-27 16:23:56 -0600

    add status index

--- data-tools/mongo-import.sh
+++ data-tools/mongo-import.sh
@@ -3,5 +3,6 @@ mongo $db --eval "db.dropDatabase()" && \
 mongoimport --db=$db --collection=stations --jsonArray --file stations.json && \
 mongoimport --db=$db --collection=files --jsonArray --file files.json && \
 mongo $db --eval "db.files.ensureIndex({date:1})" && \
-mongo $db --eval "db.files.ensureIndex({stationId:1})"
+mongo $db --eval "db.files.ensureIndex({stationId:1})" && \
+mongo $db --eval "db.files.ensureIndex({status:1})"
 
