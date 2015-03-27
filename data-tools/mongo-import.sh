cd81363000f1de1ccd77eb67a7c957c4433341f5
Marius Nita <m@mariusnita.com>
2015-03-27 12:56:10 -0600

    add some indexes after import

--- data-tools/mongo-import.sh
+++ data-tools/mongo-import.sh
@@ -1,5 +1,7 @@
 db=seismo
 mongo $db --eval "db.dropDatabase()" && \
 mongoimport --db=$db --collection=stations --jsonArray --file stations.json && \
-mongoimport --db=$db --collection=files --jsonArray --file files.json
+mongoimport --db=$db --collection=files --jsonArray --file files.json && \
+mongo $db --eval "db.files.ensureIndex({date:1})" && \
+mongo $db --eval "db.files.ensureIndex({stationId:1})"
 
