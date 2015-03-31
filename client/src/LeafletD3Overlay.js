8fc0c845fac03399bbd816ae45ae00d46f52bada
bennlich <bennlich@gmail.com>
2015-03-31 01:02:44 -0700

    Delete old code

--- client/src/LeafletD3Overlay.js
+++ client/src/LeafletD3Overlay.js
@@ -1,4 +1,3 @@
-// ~~based on http://bost.ocks.org/mike/leaflet/~~
 // based on http://bl.ocks.org/monfera/11100987
 
 // If we ever update Leaflet to bleeding edge,
@@ -16,12 +15,7 @@ class LeafletD3Overlay {
     // grab it; it handles zoom animation for you
     this.svg = d3.select(leafletMap.getContainer()).select("svg");
     
-    // this.svg = d3.select(leafletMap.getPanes().overlayPane).append("svg");
-    
     this.root = this.svg.append("g");
-
-    // leafletMap.on("viewreset", () => { this.reset(); });
-    // this.reset();
   }
 
   createGroup() {
@@ -32,26 +26,6 @@ class LeafletD3Overlay {
     var point = this.map.latLngToLayerPoint(new L.LatLng(y, x));
     return point;
   }
-
-  // reset() {
-  //   // Assume that an svg that covers the
-  //   // entire world will fit any dataset.
-  //   var topLeft = this.project( -180, 90 ),
-  //       bottomRight = this.project( 180, -90 );
-
-  //   // Pad the svg to account for icons
-  //   // that extend beyond map boundaries.
-  //   var padding = 50;
-
-  //   this.svg
-  //     .attr("width", bottomRight.x - topLeft.x + 2*padding)
-  //     .attr("height", bottomRight.y - topLeft.y + 2*padding)
-  //     .style("margin-left", topLeft.x - padding + "px")
-  //     .style("margin-top", topLeft.y - padding + "px");
-
-  //   this.root
-  //     .attr("transform", "translate(" + -(topLeft.x - padding) + "," + -(topLeft.y - padding) + ")");
-  // }
 }
 
 export { LeafletD3Overlay };
