b0c1f571e606d76142dffb7da1e88e76be1ed954
bennlich <bennlich@gmail.com>
2015-04-05 11:21:13 -0700

    Rename files

--- client/src/SeismoApp.js
+++ client/src/SeismoApp.js
@@ -3,11 +3,11 @@ import {Gradient} from "../bower_components/redfish-core/lib/Util/Gradient.js";
 var angular = window.angular;
 
 import { SeismoMain } from "./SeismoMain.ctrl.js";
-import { SeismoStationMap } from "./map/SeismoMap.svc.js";
+import { SeismoStationMap } from "./map/SeismoStationMap.svc.js";
 import { SeismoQuery } from "./SeismoQuery.svc.js";
 import { SeismoTimeNubbin } from "./seismo-query-nubbins/SeismoTimeNubbin.dir.js";
 import { SeismoQueryNubbins } from "./seismo-query-nubbins/SeismoQueryNubbins.dir.js";
-import { MapLink } from "./map/LeafletMap.dir.js";
+import { MapLink } from "./map/MapLink.dir.js";
 
 angular.module("SeismoApp", [])
   .controller("SeismoMain", SeismoMain)
