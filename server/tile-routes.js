ec51128034019e1e3458fb09c6c646fa92446240
Ubuntu <ubuntu@ip-172-31-44-191.us-west-2.compute.internal>
2015-04-09 00:03:10 +0000

    add new endpoint /tiles/loadfile that copes a file over from s3

--- server/tile-routes.js
+++ server/tile-routes.js
@@ -5,8 +5,32 @@ var Image = require("canvas").Image,
     Tiler = require("./tiler"),
     fs = require("fs");
 
+var exec = require("child_process").exec;
+
 var tiler = new Tiler();
 var cache = {};
+
+function localPath(filename) {
+  return __dirname + "/s3-local-cache/" + filename;
+}
+
+router.get("/loadfile/:filename", function(req, res) {
+  var filename = req.params.filename;
+  var path = localPath(filename);
+
+  console.log("requested file", filename);
+  if (fs.existsSync(path)) {
+    console.log("--> cached");
+    res.send({ success: true });
+  } else {
+    console.log("--> not cached");
+    console.time("s3fetch");
+    exec("aws s3 cp s3://WWSSN_Scans/" + filename + " --region us-east-1 " + path, function(err) {
+      console.timeEnd("s3fetch");
+      res.send({ success: err === null });
+    });
+  }
+});
 
 router.get("/:filename/:z/:x/:y.png", function(req, res) {
 
@@ -20,11 +44,11 @@ router.get("/:filename/:z/:x/:y.png", function(req, res) {
   
   // For the time being, always return tiles from a dummy seismogram.
   // The dummy is actually 0051574_0615_0124_04.png.
-  var filename = "dummy-seismo.png";
+  // var filename = "dummy-seismo.png";
 
   if (!cache[filename]) {
     console.time("readFile");
-    var file = fs.readFileSync(__dirname + "/" + filename);
+    var file = fs.readFileSync(localPath(filename));
     console.timeEnd("readFile");
     
     console.time("convertToImage");
