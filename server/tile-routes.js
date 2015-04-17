ebbe2d76c6aa1437fa9a61f153e7847ac86742b4
Marius Nita <m@mariusnita.com>
2015-04-17 17:06:58 -0600

    some docs, better naming

--- server/tile-routes.js
+++ server/tile-routes.js
@@ -1,11 +1,7 @@
-var express = require("express"),
-    router = express.Router();
-
-var Tiler = require("./tiler"),
-    fs = require("fs");
-
+var router = require("express").Router();
+var fs = require("fs");
 var exec = require("child_process").exec;
-
+var Tiler = require("./tiler");
 var cache = require("./cache");
 
 var tiler = new Tiler();
@@ -14,31 +10,46 @@ function localPath(filename) {
   return __dirname + "/local-file-cache/" + filename;
 }
 
-function getFile(filename, cb) {
-  console.log("getFile", filename);
+// This object maintains a map of filenames to lists of buffered requests.
+// When a file is loading from s3, any incoming requests for tiles will
+// be buffered in this object. Once the file is done copying from s3, the
+// requests are processed in bulk.
+var bufferedRequests = {};
+
+function ensureFileIsLocal(filename, cb) {
   var path = localPath(filename);
 
   if (fs.existsSync(path)) {
-    console.log("--> on local disk");
+    // file is already local
     cb(null);
   } else {
-    console.log("--> *not* on local disk");
+    // file is on s3
     console.time("s3fetch");
-    if (!loading[filename]) {
-      loading[filename] = [];
+
+    if (!bufferedRequests[filename]) {
+      // this is the first request for this file; create an object that will
+      // buffer subsequent requests.
+      bufferedRequests[filename] = [];
+
       exec("aws s3 cp s3://WWSSN_Scans/" + filename + " --region us-east-1 " + path, function(err) {
         console.timeEnd("s3fetch");
-        console.log("processing", loading[filename].length, "requests");
-        loading[filename].forEach(function(cb) { cb(err); });
-        delete loading[filename];
+        console.log("processing", bufferedRequests[filename].length, "buffered requests");
+
+        // the file's been copied locally, now process all the buffered requests
+        bufferedRequests[filename].forEach(function(requestCallback) {
+          requestCallback(err);
+        });
+
+        // this entry is no longer needed; since the file is now local,
+        // there will be no more buffering
+        delete bufferedRequests[filename];
       });
     }
-    console.log("buffering request");
-    loading[filename].push(cb);
+
+    // buffer this request for processing when the copy from s3 is done.
+    bufferedRequests[filename].push(cb);
   }
 }
-
-var loading = {};
 
 router.get("/:filename/:z/:x/:y.png", function(req, res) {
 
@@ -46,7 +57,7 @@ router.get("/:filename/:z/:x/:y.png", function(req, res) {
 
   var filename = process.env.NODE_ENV === "production" ? req.params.filename : "dummy-seismo.png";
 
-  getFile(filename, function(err) {
+  ensureFileIsLocal(filename, function(err) {
     var z = req.params.z,
         x = req.params.x,
         y = req.params.y,
