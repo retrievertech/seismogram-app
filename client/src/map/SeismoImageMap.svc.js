6f2f0ab8770f1de18fe3c6d1e3927f7450eed9cb
Marius Nita <m@mariusnita.com>
2015-05-04 14:17:02 +0300

    put metadata layers on the map

--- client/src/map/SeismoImageMap.svc.js
+++ client/src/map/SeismoImageMap.svc.js
@@ -27,10 +27,65 @@ var imageMapCRS = L.extend({}, L.CRS.Simple, {
 
 class SeismoImageMap {
   
-  constructor($http, SeismoServer) {
+  constructor($http, $q, SeismoServer) {
     this.server = SeismoServer;
     this.http = $http;
+    this.q = $q;
     this.leafletMap = null;
+    this.metadataLayers = [
+      {
+        name: "Region of Interest",
+        key: "roi",
+        on: true,
+        zIndex: 10,
+        leafletLayer: null,
+        style: {
+          color: "blue",
+          weight: 1,
+          opacity: 0.9
+        }
+      }, {
+        name: "Mean Lines",
+        key: "meanlines",
+        on: true,
+        zIndex: 11,
+        leafletLayer: null,
+        style: {
+          color: "yellow",
+          weight: 1,
+          opacity: 0.9
+        }
+      }, {
+        name: "Intersectons",
+        key: "intersections",
+        on: true,
+        zIndex: 12,
+        leafletLayer: null,
+        style: {
+          pointToLayer: function(feature, latlng) {
+            return L.circleMarker(latlng, {
+              fillColor: "red",
+              color: "white",
+              weight: 1,
+              opacity: 0.9,
+              radius: feature.properties.radius
+            });
+          }
+        }
+      }, {
+        name: "Segments",
+        key: "segments",
+        on: true,
+        zIndex: 13,
+        leafletLayer: null,
+        style: {
+          color: "red",
+          weight: 1,
+          opacity: 0.9
+        }
+      }
+    ];
+
     this.imageLayer = null;
     this.imageLayerOpts = {
       // tileSize: 512,
@@ -54,27 +109,42 @@ class SeismoImageMap {
     
     leafletMap.setView(new L.LatLng(2000, 7000), 2);
   }
-
+  
   loadImage(imagename) {
-    var s3Prefix = "https://s3.amazonaws.com/wwssn-metadata/040471_0638_0023_04/";
+    var s3Prefix = "https://s3.amazonaws.com/wwssn-metadata/010162_1742_0007_04/";
     var url = this.server.tilesUrl + "/" + imagename + "/{z}/{x}/{y}.png";
 
     // lazy initialization
     if (!this.imageLayer) {
       this.imageLayer = L.tileLayer(url, this.imageLayerOpts)
         .addTo(this.leafletMap);
-      // quick overlay of meanlines geojson
-      this.http({url: s3Prefix + "meanlines.json"}).then((ret) => {
-        console.log(ret.data);
-        L.geoJson(ret.data, {
-          color: "red",
-          weight: 2,
-          opacity: 0.9
-        }).addTo(this.leafletMap);
-      });
-      return;
+    } else {
+      this.imageLayer.setUrl(url);
     }
-    this.imageLayer.setUrl(url);
+
+    // remove metadata layers from the map if any
+    this.metadataLayers.forEach((layer) => {
+      if (this.leafletMap.hasLayer(layer.leafletLayer)) {
+        this.leafletMap.removeLayer(layer.leafletLayer);
+      }
+    });
+
+    // load the data and recreate the layers
+    var promises = this.metadataLayers.map((layer) => {
+      return this.http({url: s3Prefix + layer.key + ".json"}).then((ret) => {
+        console.log(layer.key + ":", ret.data);
+        layer.leafletLayer = L.geoJson(ret.data, layer.style);
+      });
+    });
+
+    // when all the data is loaded, put it on the map
+    this.q.all(promises).then(() => {
+      this.metadataLayers.forEach((layer) => {
+        if (layer.on) {
+          this.leafletMap.addLayer(layer.leafletLayer, {zIndex: layer.zIndex});
+        }
+      });
+    });
   }
 
 }
