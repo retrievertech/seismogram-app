5daf700308182c90b833304bac2e93c0e81a2716
Marius Nita <m@mariusnita.com>
2015-05-09 13:01:49 +0300

    split off the CRS code

---
+++ client/src/map/ImageMapCRS.js
@@ -0,0 +1,28 @@
+var L = window.L;
+
+// This has to agree with the same value in server/tiler.js
+var imageCoordExtent = Math.pow(2, 15);
+// Assume 256px tiles
+var scale = 256 / imageCoordExtent;
+
+// Translation between tile coords and image coords
+var imageMapProjection = {
+  project: function (latlng) {
+    return new L.Point(latlng.lng * scale, latlng.lat * scale);
+  },
+
+  unproject: function (point) {
+    return new L.LatLng(point.y / scale, point.x / scale);
+  },
+
+  // Defines a coordinate space with (0,0) in the top-left
+  bounds: L.bounds([0, imageCoordExtent], [0, imageCoordExtent])
+};
+
+// Defines a flat (null transformation) coordinate space with (0,0) in the top-left.
+var ImageMapCRS = L.extend({}, L.CRS.Simple, {
+  projection: imageMapProjection,
+  transformation: new L.Transformation(1, 0, 1, 0)
+});
+
+export { ImageMapCRS };
--- client/src/map/SeismoImageMap.svc.js
+++ client/src/map/SeismoImageMap.svc.js
@@ -1,29 +1,5 @@
+import { ImageMapCRS } from "./ImageMapCRS.js";
 var L = window.L;
-
-// This has to agree with the same value in server/tiler.js
-var imageCoordExtent = Math.pow(2, 15);
-// Assume 256px tiles
-var scale = 256 / imageCoordExtent;
-
-// Translation between tile coords and image coords
-var imageMapProjection = {
-  project: function (latlng) {
-    return new L.Point(latlng.lng * scale, latlng.lat * scale);
-  },
-
-  unproject: function (point) {
-    return new L.LatLng(point.y / scale, point.x / scale);
-  },
-
-  // Defines a coordinate space with (0,0) in the top-left
-  bounds: L.bounds([0, imageCoordExtent], [0, imageCoordExtent])
-};
-
-// Defines a flat (null transformation) coordinate space with (0,0) in the top-left.
-var imageMapCRS = L.extend({}, L.CRS.Simple, {
-  projection: imageMapProjection,
-  transformation: new L.Transformation(1, 0, 1, 0)
-});
 
 var IntersectionCircle = L.CircleMarker.extend({
   getRadius: function(zoom, feature) {
@@ -117,7 +93,7 @@ class SeismoImageMap {
     var leafletMap = this.leafletMap = L.map(id, {
       maxZoom: 7,
       minZoom: 0,
-      crs: imageMapCRS,
+      crs: ImageMapCRS,
       editable: true,
       editOptions: {
         skipMiddleMarkers: true
