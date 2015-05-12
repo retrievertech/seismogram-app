aaf6299e62e9552302dd412dded5aa2c816c164e
Marius Nita <m@mariusnita.com>
2015-05-12 14:57:49 +0300

    more aggressive map invalidation

--- client/src/map/MapLink.dir.js
+++ client/src/map/MapLink.dir.js
@@ -7,7 +7,22 @@ class MapLink {
 
         if (attrs.mapLink === "SeismoImageMap") {
           scope.$watch("editing", (val, oldVal) => {
+            // invalidate size when going in and out of editing mode
             if (val !== oldVal) {
+              $timeout(() => mapService.leafletMap.invalidateSize());
+            }
+          });
+
+          scope.$watch("imageMapVisible", (val, oldVal) => {
+            // invalidate size when the image map becomes visible
+            if (val && !oldVal) {
+              $timeout(() => mapService.leafletMap.invalidateSize());
+            }
+          });
+        } else {
+          scope.$watch("imageMapVisible", (val, oldVal) => {
+            // invalidate size when the pies map becomes visible
+            if (!val && oldVal) {
               $timeout(() => mapService.leafletMap.invalidateSize());
             }
           });
