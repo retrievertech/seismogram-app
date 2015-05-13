a355a1cb43bf06bd8e841b7d890eeeb438dee283
bennlich <bennlich@gmail.com>
2015-05-13 14:42:44 -0700

    Use isolate scope to watch for changes to viewReset object

--- client/src/map/MapLink.dir.js
+++ client/src/map/MapLink.dir.js
@@ -1,34 +1,17 @@
 class MapLink {
   constructor($timeout) {
     return {
+      scope: {
+        mapLink: '=', // call this object's init function when the directive links
+        viewReset: '=' // whenever this object changes, invalidate map size
+      },
       link: (scope, element, attrs) => {
-        var mapService = scope[attrs.mapLink];
-        mapService.init(attrs.id);
-
-        if (attrs.mapLink === "SeismoImageMap") {
-          scope.$watch("editing", (val, oldVal) => {
-            // invalidate size when going in and out of editing mode
-            if (val !== oldVal) {
-              $timeout(() => mapService.leafletMap.invalidateSize());
-            }
-          });
-
-          scope.$watch("imageMapVisible", (val, oldVal) => {
-            // invalidate size when the image map becomes visible
-            if (val && !oldVal) {
-              $timeout(() => mapService.leafletMap.invalidateSize());
-            }
-          });
-        } else {
-          scope.$watch("imageMapVisible", (val, oldVal) => {
-            // invalidate size when the pies map becomes visible
-            if (!val && oldVal) {
-              $timeout(() => mapService.leafletMap.invalidateSize());
-            }
-          });
-        }
+        scope.mapLink.init(attrs.id);
+        scope.$watch('viewReset', () => {
+          $timeout(() => mapService.leafletMap.invalidateSize());
+        });
       }
-    };
+    }
   }
 }
 
