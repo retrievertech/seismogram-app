a45a3518ad647713a75d2d35afd658b483f1c0f6
Marius Nita <m@mariusnita.com>
2015-05-14 12:44:55 +0300

    oops mapService is undefined

--- client/src/map/MapLink.dir.js
+++ client/src/map/MapLink.dir.js
@@ -2,16 +2,17 @@ class MapLink {
   constructor($timeout) {
     return {
       scope: {
-        mapLink: '=', // call this object's init function when the directive links
-        viewReset: '=' // whenever this object changes, invalidate map size
+        mapLink: "=", // call this object's init function when the directive links
+        viewReset: "=" // whenever this object changes, invalidate map size
       },
       link: (scope, element, attrs) => {
         scope.mapLink.init(attrs.id);
-        scope.$watch('viewReset', () => {
-          $timeout(() => mapService.leafletMap.invalidateSize());
+        scope.$watch("viewReset", (val) => {
+          console.log(val);
+          $timeout(() => scope.mapLink.leafletMap.invalidateSize());
         });
       }
-    }
+    };
   }
 }
 
