4e9cec9f15348bac132344cc7a9b365afb0fda38
Marius Nita <m@mariusnita.com>
2015-05-19 16:09:04 +0300

    "show log" button and log div

--- client/src/SeismoMain.ctrl.js
+++ client/src/SeismoMain.ctrl.js
@@ -50,6 +50,35 @@ class SeismoMain {
                       SeismoStatus.is(file.status, "Edited"));
     };
 
+    $scope.logShowing = false;
+    $scope.log = "";
+
+    $scope.hasLog = () => {
+      var file = SeismoImageMap.currentFile;
+      return file && (SeismoStatus.is(file.status, "Complete") ||
+                      SeismoStatus.is(file.status, "Edited") ||
+                      SeismoStatus.is(file.status, "Failed"));
+    };
+
+    $scope.showLog = () => {
+      var file = SeismoImageMap.currentFile;
+      var url = "logs/" + file.name + ".txt";
+
+      $scope.log = "";
+
+      $http({url: url}).then((res) => {
+        $scope.log = res.data;
+      }).catch(() => {
+        $scope.log = "A log is not available for this file...";
+      }).then(() => {
+        $scope.logShowing = true;
+      });
+    };
+
+    $scope.hideLog = () => {
+      $scope.logShowing = false;
+    };
+
     $scope.imageMapVisible = false;
 
     $scope.showImageMap = () => {
