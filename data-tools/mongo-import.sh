98bfb79c2e32dd2d5a6e893d5f935c944dab6613
Marius Nita <m@mariusnita.com>
2015-05-22 17:43:42 +0300

    distinguish between dev and prod

--- data-tools/mongo-import.sh
+++ data-tools/mongo-import.sh
@@ -5,5 +5,9 @@ mongoimport --db=$db --collection=files --jsonArray --file files.json && \
 mongo $db --eval "db.files.ensureIndex({date:1})" && \
 mongo $db --eval "db.files.ensureIndex({stationId:1})" && \
 mongo $db --eval "db.files.ensureIndex({status:1})" && \
-files_with_metadata=`aws s3 ls "s3://wwssn-metadata/" | perl -e '@lines = <>; @lines = map { s/^.*PRE\s//; s/[\/\s]*$//; "\"$_\"" } @lines; print join ",", @lines'` && \
+if [ "$1" != "dev" ]; then
+  files_with_metadata=`aws s3 ls "s3://wwssn-metadata/" | perl -e '@lines = <>; @lines = map { s/^.*PRE\s//; s/[\/\s]*$//; "\"$_\"" } @lines; print join ",", @lines'`
+else
+  files_with_metadata=`ls -1 ../client/metadata | perl -e '@lines = <>; @lines = map { s/[\/\s]*$//; "\"$_\"" } @lines; print join ",", @lines'`
+fi && \
 mongo $db --eval 'db.files.update({name: {$in: ['$files_with_metadata']}}, {$set: {status:3}}, {multi:true})'
