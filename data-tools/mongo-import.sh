5c73625ab2a0ae66b73026c7d89321386dcbc375
Marius Nita <m@mariusnita.com>
2015-05-22 14:29:21 +0000

    update status for files with metadata, when file jsons are reimported

--- data-tools/mongo-import.sh
+++ data-tools/mongo-import.sh
@@ -1,8 +1,9 @@
-db=seismo
+db=seismo && \
 mongo $db --eval "db.dropDatabase()" && \
 mongoimport --db=$db --collection=stations --jsonArray --file stations.json && \
 mongoimport --db=$db --collection=files --jsonArray --file files.json && \
 mongo $db --eval "db.files.ensureIndex({date:1})" && \
 mongo $db --eval "db.files.ensureIndex({stationId:1})" && \
-mongo $db --eval "db.files.ensureIndex({status:1})"
-
+mongo $db --eval "db.files.ensureIndex({status:1})" && \
+files_with_metadata=`aws s3 ls "s3://wwssn-metadata/" | perl -e '@lines = <>; @lines = map { s/^.*PRE\s//; s/[\/\s]*$//; "\"$_\"" } @lines; print join ",", @lines'` && \
+mongo $db --eval 'db.files.update({name: {$in: ['$files_with_metadata']}}, {$set: {status:3}}, {multi:true})'
