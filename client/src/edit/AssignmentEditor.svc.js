021f83894501d988b8b56a481676be527d76b454
Marius Nita <m@mariusnita.com>
2015-07-31 13:25:17 -0600

    add assignment editor svc

---
+++ client/src/edit/AssignmentEditor.svc.js
@@ -0,0 +1,44 @@
+export class AssignmentEditor {
+  constructor($http, SeismogramMap, ServerUrls, ScreenMessage, Popup) {
+    this.$http = $http;
+    this.SeismogramMap = SeismogramMap;
+    this.ServerUrls = ServerUrls;
+    this.ScreenMessage = ScreenMessage;
+    this.Popup = Popup;
+    this.editing = false;
+  }
+
+  startEditing() {
+    this.editing = true;
+  }
+
+  stopEditing() {
+    this.editing = false;
+  }
+
+  autoAssign() {
+    var segments = this.SeismogramMap.getLayer("segments");
+    var meanlines = this.SeismogramMap.getLayer("meanlines");
+
+    var request = {
+      method: "POST",
+      url: this.ServerUrls.assignUrl,
+      data: {
+        segments: JSON.stringify(segments.leafletLayer.toGeoJSON()),
+        meanlines: JSON.stringify(meanlines.leafletLayer.toGeoJSON())
+      }
+    };
+
+    this.ScreenMessage.start("Running Assignment. Please wait...");
+
+    this.$http(request).then((res) => {
+      this.ScreenMessage.stop("Running Assignment. Please wait...");
+      this.ScreenMessage.ephemeral("Success.", "simple", 2000);
+      console.log("got data", res.data);
+    }).catch((res) => {
+      this.ScreenMessage.stop("Running Assignment. Please wait...");
+      this.ScreenMessage.ephemeral("Error! Sorry!", "error", 3000);
+      console.log("error", res);
+    });
+  }
+}
