eed8e00e1d7781618b9672957a04ac282ef26324
bennlich <bennlich@gmail.com>
2015-10-08 16:52:12 -0700

    fix ec2 import

--- data-tools/import-ec2-file-data.sh
+++ data-tools/import-ec2-file-data.sh
@@ -1,5 +1,5 @@
 db=seismo
-login=ubuntu@52.10.53.123
+login=ubuntu@seismo.redfish.com
 
 ssh $login "mongoexport -d $db -c files --jsonArray -o mongo.json" && \
 ssh $login "tar zcvf mongo.json.tar.gz mongo.json" && \
@@ -7,5 +7,5 @@ scp $login:./mongo.json.tar.gz . && \
 ssh $login "rm -f mongo.json.tar.gz mongo.json" && \
 tar zxvf mongo.json.tar.gz && \
 mongo $db --eval "db.files.remove({})" && \
-mongoimport -d $db -c files --jsonArray --file mongo.json && \
+mongoimport --host=127.0.0.1 -d $db -c files --jsonArray --file mongo.json && \
 rm -f mongo.json.tar.gz mongo.json
