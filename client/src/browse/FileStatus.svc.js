6279a26dbd936020e33865905ecbce6d52351611
bennlich <bennlich@gmail.com>
2015-10-29 16:14:53 -0700

    Add the "problematic" status.

--- client/src/browse/FileStatus.svc.js
+++ client/src/browse/FileStatus.svc.js
@@ -1,24 +1,42 @@
 export class FileStatus {
   constructor() {
-    this.statuses = [{
-      code: 0,
-      name: "No Data"
-    },
-    // {
-    //  code: 1,
-    //  name: "Processing"
-    //},
-    {
-     code: 2,
-     name: "Failed"
-    },
-    {
-      code: 3,
-      name: "Has Raw Data"
-    }, {
-      code: 4,
-      name: "Has Edited Data"
-    }];
+    this.statuses = [
+      {
+        code: 0,
+        name: "No Data"
+      },
+      // It's pretty pointless to be able to show
+      // seismos that are currently being processed
+      // (it's such a temporary state), so we hide
+      // it from the query filter UI.
+      //
+      // {
+      //  code: 1,
+      //  name: "Processing"
+      //},
+      {
+       code: 2,
+       name: "Failed"
+      },
+      {
+        code: 3,
+        name: "Processed"
+      },
+      {
+        code: 4,
+        name: "Edited"
+      }
+      // Uncomment the "problematic" code below
+      // if you want to be able to search for
+      // seismograms that were processed without error
+      // but have been flagged for issues (e.g. too many segments).
+      //
+      // ,
+      // {
+      //   code: 5,
+      //   name: "Problematic"
+      // }
+    ];
   }
 
   getStatus(name) {
