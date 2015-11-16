88252f59b835076dfe7a4f0e3a2b17dc6dabd73d
bennlich <bennlich@gmail.com>
2015-11-16 15:57:13 -0800

    Delete old files beyond a limit (prevent running out of disk space)

--- server/disk-cache.js
+++ server/disk-cache.js
@@ -1,6 +1,7 @@
 var fs = require("fs");
 var exec = require("child_process").exec;
 var escape = require("./util").escape;
+var async = require("async");
 
 function localPath(filename) {
   return __dirname + "/local-file-cache/" + filename;
@@ -12,6 +13,55 @@ function localPath(filename) {
 // requests are processed in bulk.
 var bufferedRequests = {};
 
+// No more than this number of files are allowed to be saved locally.
+// (Prevents server from running out of disk space.)
+var MAX_LOCAL_FILES = 10;
+
+function deleteOldFiles(cb) {
+  var filenames = fs
+    .readdirSync(localPath(""))
+    .filter(function(filename) {
+      return new RegExp(/.*\.png/).test(filename);
+    });
+
+  console.log(filenames.length, "files in local-file-cache");
+
+  if (filenames.length < MAX_LOCAL_FILES) {
+    if (typeof cb === "function") {
+      cb(null);
+    }
+  } else {
+    var fullPaths = filenames.map(function(filename) {
+      return localPath(filename);
+    });
+
+    async.map(fullPaths, fs.stat, function(err, results) {
+      if (err) console.log(err);
+
+      var numFilesToDelete = filenames.length - MAX_LOCAL_FILES + 1;
+      console.log("deleting", numFilesToDelete, "files");
+      
+      // insert filenames into fs.stat results
+      results.forEach(function(info, idx) {
+        info.path = fullPaths[idx];
+      });
+
+      // delete oldest files first
+      results.sort(function(fileA, fileB) {
+        return fileA.birthtime.getTime() - fileB.birthtime.getTime();
+      });
+
+      var pathsToDelete = results
+        .slice(0, numFilesToDelete)
+        .map(function(file) { return file.path });
+
+      async.map(pathsToDelete, fs.unlink, function(err) {
+        if (err) console.log(err);
+      });
+    });
+  }
+}
+
 function ensureFileIsLocal(filename, cb) {
   var path = localPath(filename);
 
@@ -20,45 +70,47 @@ function ensureFileIsLocal(filename, cb) {
     // file is already local
     cb(null);
   } else {
-    // file is on s3
-    console.time("s3fetch");
+    deleteOldFiles(function() {
+      // file is on s3
+      console.time("s3fetch");
 
-    if (!bufferedRequests[filename]) {
-      // this is the first request for this file; create an object that will
-      // buffer subsequent requests.
-      bufferedRequests[filename] = [];
+      if (!bufferedRequests[filename]) {
+        // this is the first request for this file; create an object that will
+        // buffer subsequent requests.
+        bufferedRequests[filename] = [];
 
-      var command = "aws s3 cp s3://WWSSN_Scans/" + filename + " --region us-east-1 " + escape(path);
+        var command = "aws s3 cp s3://WWSSN_Scans/" + filename + " --region us-east-1 " + escape(path);
 
-      if (process.env.NODE_ENV !== "production") {
-        command = "wget http://s3.amazonaws.com/WWSSN_Scans/" + filename + " -O " + escape(path);
-      }
-
-      console.log(command);
-
-      exec(command, function(err) {
-        console.timeEnd("s3fetch");
-
-        if (err) {
-          console.log("error copying file", err);
-          return;
+        if (process.env.NODE_ENV !== "production") {
+          command = "wget http://s3.amazonaws.com/WWSSN_Scans/" + filename + " -O " + escape(path);
         }
 
-        console.log("processing", bufferedRequests[filename].length, "buffered requests");
+        console.log(command);
 
-        // the file's been copied locally, now process all the buffered requests
-        bufferedRequests[filename].forEach(function(requestCallback) {
-          requestCallback(err);
+        exec(command, function(err) {
+          console.timeEnd("s3fetch");
+
+          if (err) {
+            console.log("error copying file", err);
+            return;
+          }
+
+          console.log("processing", bufferedRequests[filename].length, "buffered requests");
+
+          // the file's been copied locally, now process all the buffered requests
+          bufferedRequests[filename].forEach(function(requestCallback) {
+            requestCallback(err);
+          });
+
+          // this entry is no longer needed; since the file is now local,
+          // there will be no more buffering
+          delete bufferedRequests[filename];
         });
+      }
 
-        // this entry is no longer needed; since the file is now local,
-        // there will be no more buffering
-        delete bufferedRequests[filename];
-      });
-    }
-
-    // buffer this request for processing when the copy from s3 is done.
-    bufferedRequests[filename].push(cb);
+      // buffer this request for processing when the copy from s3 is done.
+      bufferedRequests[filename].push(cb);
+    });
   }
 }
 
